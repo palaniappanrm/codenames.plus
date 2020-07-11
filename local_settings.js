5ed3debd009a6e4add75497c29eeb6b5db6193b2
Daniel Perelman <perelman@cs.washington.edu>
2020-07-11 04:40:00 -0700

    Add local settings file for green/blue color scheme.

---
+++ local_settings.js
@@ -0,0 +1,10 @@
+var config = module.exports = {
+  redPalette: [
+    {name: "green", deep: "#008000", light: "rgb(168, 235, 216)"},
+    {name: "red", deep: "#B32728", light: "rgb(236, 170, 170)"},
+  ],
+  bluePalette: [
+    {name: "blue", deep: "#11779F", light: "rgb(168, 216, 235)"},
+    {name: "purple", deep: "#AA69DD", light: "rgb(216, 191, 216)"},
+  ],
+}
