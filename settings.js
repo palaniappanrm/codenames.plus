110ebc63eeaa0ba5686ca2a91f8b4dd04d86178f
Daniel Perelman <perelman@cs.washington.edu>
2020-07-11 03:50:58 -0700

    Add palettes to settings.js.

--- settings.js
+++ settings.js
@@ -16,4 +16,13 @@ var config = module.exports = {
   // Set to non-null to use fixed team names instead of using color names
   redTeamName: null,
   blueTeamName: null,
+
+  redPalette: [
+    {name: "red", deep: "#B32728", light: "rgb(236, 170, 170)"},
+    {name: "purple", deep: "#AA69DD", light: "rgb(216, 191, 216)"},
+  ],
+  bluePalette: [
+    {name: "blue", deep: "#11779F", light: "rgb(168, 216, 235)"},
+    {name: "green", deep: "#008000", light: "rgb(168, 235, 216)"},
+  ],
 }
