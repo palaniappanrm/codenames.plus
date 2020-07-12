9866eca516f09915eb6a1a05a6b982fc16c1b495
Daniel Perelman <perelman@cs.washington.edu>
2020-07-11 22:06:36 -0700

    Change default back to red vs. blue but keep green vs. blue as an option.

--- local_settings.js
+++ local_settings.js
@@ -1,8 +1,8 @@
 var config = module.exports = {
   timeout: 3600, // one hour AFK timeout
   redPalette: [
-    {name: "green", deep: "#008000", light: "rgb(168, 235, 216)"},
     {name: "red", deep: "#B32728", light: "rgb(236, 170, 170)"},
+    {name: "green", deep: "#008000", light: "rgb(168, 235, 216)"},
   ],
   bluePalette: [
     {name: "blue", deep: "#11779F", light: "rgb(168, 216, 235)"},
